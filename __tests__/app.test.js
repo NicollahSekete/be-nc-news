58d2b5b790b7a915b0c74a9844ab03f7d30543ea
NicollahSeketeIQ <nicollah.sekete@uniqueiq.co.uk>
2023-02-24 11:40:40 +0000

    ticket 13 complete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,14 @@ const app = require('../app')
 const db = require('../db/connection.js')
 const seed = require('../db/seeds/seed')
 const testData = require('../db/data/test-data')
-const { text } = require('express')
+const endpoints_json = require("../endpoints.json");
+
 
 beforeEach(() => seed(testData))
 afterAll(() => db.end())
 
 describe("app", () => {
-    describe("/api", () => {
+    describe("*", () => {
         test("should return 404 when route does not exist", () => {
             return request(app).get('/api/topic').expect(404).then((res) => {
                 expect(res.body.msg).toBe('Path not found')
@@ -407,5 +408,15 @@ describe("app", () => {
                 expect(body.msg).toBe('Bad Request')
             })
         })
+    })
+
+    describe("GET /api", () => {
+        test("should return an object describing all available endpoints", () => {
+            return request(app).get('/api').expect(200).then(({ body }) => {
+                const { api } = body;
+                expect(typeof api).toBe("object")
+                expect(api).toEqual(endpoints_json)
+            })
+        });
     })
 });
